4a45b0fd597e77bb3255e2d224768c13c773b94b
Justin Riley <20hz20khz@gmail.com>
2021-10-05 19:41:13 -0400

    Update neopixel-show-8-24-21.ino

--- neopixel-show-8-24-21.ino
+++ neopixel-show-8-24-21.ino
@@ -24,23 +24,7 @@
 // How many NeoPixels are attached to the Arduino?
 #define LED_COUNT 30 * STRANDS
 
-#define BLACK = strip.Color(0,0,0);
-#define BLUE = strip.Color(0,0,225);
-#define BROWN = strip.Color(175,130,100);
-#define CYAN = strip.Color(0,255,255);
-#define GREEN = strip.Color(30, 220,60);
-#define GOLD = strip.Color(255,160,0);
-#define LIGHTBLUE = strip.Color(135,206,250);
-#define LIGHTGREEN = strip.Color(0,225,50);
-#define LIGHTPURPLE = strip.Color(230,100,250);
-#define MAGENTA = strip.Color(255,0,255);
-#define ORANGE = strip.Color(255,130,0);
-#define PINK = strip.Color(255,50,150);
-#define PURPLE = strip.Color(160,0,255);
-#define RED = strip.Color(255,0,0);
-#define SILVER = strip.Color(225,225,230);
-#define WHITE = strip.Color(255,255,255);
-#define YELLOW = strip.Color(255,255,0);
+
 
 
 // Declare our NeoPixel strip object:
@@ -53,6 +37,24 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+uint32_t BLACK = strip.Color(0,0,0);
+uint32_t BLUE = strip.Color(0,0,225);
+uint32_t BROWN = strip.Color(175,130,100);
+uint32_t CYAN = strip.Color(0,255,255);
+uint32_t GREEN = strip.Color(30, 220,60);
+uint32_t GOLD = strip.Color(255,160,0);
+uint32_t LIGHTBLUE = strip.Color(135,206,250);
+uint32_t LIGHTGREEN = strip.Color(0,225,50);
+uint32_t LIGHTPURPLE = strip.Color(230,100,250);
+uint32_t MAGENTA = strip.Color(255,0,255);
+uint32_t ORANGE = strip.Color(255,130,0);
+uint32_t PINK = strip.Color(255,50,150);
+uint32_t PURPLE = strip.Color(160,0,255);
+uint32_t RED = strip.Color(255,0,0);
+uint32_t SILVER = strip.Color(225,225,230);
+uint32_t WHITE = strip.Color(255,255,255);
+uint32_t YELLOW = strip.Color(255,255,0);
 
 
 // setup() function -- runs once at startup --------------------------------
